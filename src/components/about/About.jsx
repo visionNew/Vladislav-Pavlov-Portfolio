e68519e46764087178a0c2e658872a0d593167f1
Vladislav  Pavlov <45560934+visionNew@users.noreply.github.com>
2023-02-03 14:34:03 +0200

    First Commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,15 +1,14 @@
 import './about.css';
-import React,{useState} from 'react'
-import IMGAbout from '../../assets/vp-person-about.png'
-import IMGCert from '../../assets/certificate.png'
-import CV from '../../assets/CV-VZP.pdf'
-import {FaDownload, FaRegWindowClose} from 'react-icons/fa'
-import {AiOutlineSafetyCertificate} from 'react-icons/ai'
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { FaDownload } from 'react-icons/fa';
+import { AiOutlineSafetyCertificate } from 'react-icons/ai';
+import CV from '../../assets/CV-VZP.pdf';
+import IMGAbout from '../../assets/vp-person-about.png';
+import IMGCert from '../../assets/certificate.png';
 
 const About = () => {
       const { t } = useTranslation();
-
       const [isOpen, setIsOpen] = useState(false);
       const togglePopup = () => { setIsOpen(!isOpen); }
       const closePopup =() => { setIsOpen(false)}
@@ -32,16 +31,14 @@ const About = () => {
                 </p>
                 <div className="about__btns">
                   <a href={CV} download className='btn'>{t('about-btn.1')} <FaDownload /></a>
-               
                   <button onClick={togglePopup} className='btn'>{t('about-btn.2')} <AiOutlineSafetyCertificate className='btn-icon' /></button>  
                 </div>
                 
                 <div>
                   {
-                    isOpen && <div className="popup__container">
+                    isOpen && <div onClick = {closePopup} className="popup__container">
                                   <div className= "popup__body scale-in-center" >
                                       <img src={IMGCert} alt="Certificate" />
-                                      <button onClick = {closePopup} ><FaRegWindowClose/></button>
                                   </div>
                               </div>
                   }
