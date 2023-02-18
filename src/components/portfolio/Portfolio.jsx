6853e595125ff6386a3b9824e2814554aa2d041b
Vladislav  Pavlov <45560934+visionNew@users.noreply.github.com>
2023-02-18 20:28:01 +0200

    First commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,24 +1,48 @@
 import './portfolio.css'; // CSS FILE FOR COMPONENT
 import { cardData } from '../../utils/data'; // DATA FILE FOR COMPONENT
-import { useState } from "react"; // REACT HOOKS
+import { useState, useEffect } from "react"; // REACT HOOKS
 import { useTranslation } from 'react-i18next'; // TRANSLATION FILE
 
 const Portfolio = () => {
   const { t } = useTranslation(); // FUNCTION TRANSLATION PAGE
-// START FUNCTION FOR LOAD MORE BUTTON
-  const [showMore,setShowMore] = useState(3); 
+// START FUNCTION FOR LOAD MORE BUTTON AND FILTER BUTTONS
+  const [ showMore, setShowMore ] = useState(3); 
+  const [ data, setData ] = useState([]);
+  const [ buttons, setButtons ] = useState([]);
+  
+  const slice = data.slice(0, showMore);
   const loadMore = () =>{ setShowMore(showMore + 3);}
-  const slice = cardData.slice(0, showMore);
-// END FUNCTION FOR LOAD MORE BUTTON
+
+  useEffect(()=>{
+    setData(cardData);
+    setButtons(['All', ...new Set(cardData.map((item)=> item.category))])
+  },[]) 
+
+  const filter = (itemData) =>{
+  if(itemData === 'All'){
+    setData(cardData);
+    return;
+  }
+  const filterData = cardData.filter((item)=> item.category === itemData);
+  setData(filterData);
+}
+// END FUNCTION FOR LOAD MORE BUTTON AND FILTER BUTTONS
   return (
     <section id="portfolio">
       {/* ============== Start Portfolio Section ============== */}
         <h2>{t('portfolio-h2')}</h2> {/* ============== Portfolio H2 ============== */}
         <div className="container portfolio__container">
         {/* ============== Start Portfolio Card ============== */}
-          <div className="portfolio__cards">
+        {/* ============== Start Portfolio Filter Buttons ============== */}
+          <div className="filter__btn blur">
+              {
+                buttons.map((item, i)=> <button className="btn" onClick={()=>{filter(item)}}>{item}</button>)
+              }
+          </div>
+        {/* ============== End Portfolio Filter Buttons ============== */}
+          <div className="portfolio__cards blur">
             { // PORTFOLIO CARD DATA MAP
-              slice.map(item => <article className={item.class} key={item.id}>
+              slice.map(item => <article className="portfolio__item scale-in-center" key={item.id}>
                                 {/* ============== Start Card Image ============== */} 
                                   <div className="portfolio__item-image">
                                     <img src={require('../../assets/project/' + item.img_name + '.png')} alt="Project Img" />
